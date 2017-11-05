942138c7ebcbf549d5e8e2d7b607f9478d1e4865
wangziwen <417805862@qq.com>
2017-11-05 17:06:51 +0800

    SpaWebDemo工程

---
+++ Context/KevenspaDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpaWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaWeb.Context
+{
+    public class KevenspaDemoContext : DbContext
+    {
+        public KevenspaDemoContext(DbContextOptions<KevenspaDemoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> db_users { get; set; }
+    }
+
+}
---
+++ Controllers/KevenController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaWeb.Context;
+
+namespace SpaWeb.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/keven")]
+    public class KevenController : Controller
+    {
+        private KevenspaDemoContext _context;
+        public KevenController (KevenspaDemoContext context)
+        {
+            _context = context;
+        }
+
+        //Get:
+        [HttpGet("get")]
+        public IActionResult Get()
+        {
+            return Ok(_context.db_users.ToList());
+        }
+
+        [HttpGet("addtest")]
+        public IActionResult AddTest()
+        {
+            _context.db_users.Add(new Models.User()
+            {
+                Name = DateTime.Now.ToShortDateString(),
+                Age = 10,
+                Country = ""
+            });
+            _context.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
